eba21aa4bace2d8e327e493ede8864446501fa57
Den <dimas.dermawan@ui.ac.id>
2023-03-30 22:01:18 +0700

    Fix navbar, landing page carousel

--- src/components/lists/Lists.jsx
+++ src/components/lists/Lists.jsx
@@ -2,11 +2,20 @@ import React from "react"
 import { Link } from "react-router-dom"
 
 function Lists({ lists = [""], ...props }) {
-  return lists.map((list, i) => (
-    <li key={i} className={"text-light text-sm font-semibold " + props.style}>
-      <Link to={list.toLowerCase().replace(" ", "-")}>{list}</Link>
-    </li>
-  ))
+    return lists.map((list, i) => (
+        <li key={i} className={props.liststyle}>
+            <Link
+                to={list.toLowerCase().replace(" ", "-")}
+                className={
+                    "block w-full h-full text-light text-sm font-semibold " +
+                    props.textstyle
+                }
+                onClick={props.onClick}
+            >
+                {list}
+            </Link>
+        </li>
+    ))
 }
 
 export default Lists
--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom"
 import logo from "../../assets/images/exercise-logos.svg"
 
 function Logo({ ...props }) {
-  return (
-    <Link id="exercise-logo" to="/" className="w-full">
-      <img
-        alt="Exercise's Logo"
-        src={logo}
-        className={props.size ? `w-${props.size}` : "w-24"}
-      />
-    </Link>
-  )
+    return (
+        <Link id="exercise-logo" to="/" onClick={props.onClick}>
+            <img
+                alt="Exercise's Logo"
+                src={logo}
+                className={props.size ? `w-${props.size}` : "w-24"}
+            />
+        </Link>
+    )
 }
 
 export default Logo
