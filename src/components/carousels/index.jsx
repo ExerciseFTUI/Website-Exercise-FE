79296947d5fefa622bbb7b37418a00b0f0eaff2b
Den <dimas.dermawan@ui.ac.id>
2023-04-01 11:32:20 +0700

    Merging carousels component to index

---
+++ src/components/carousels/index.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+import AutoSlider from "./AutoSlider"
+import Carousel from "./Carousel"
+
+export {
+  AutoSlider,
+  Carousel
+}
--- src/pages/Landing/Carousel.jsx
+++ src/pages/Landing/Carousel.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
-import AutoSlider from "../../components/carousel/AutoSlider"
+import { AutoSlider } from "../../components/carousels"
 
 import car1 from "../../assets/images/landing/carousel/be-our-client.png"
 
