3260bf9f5492d6714c2063a0d9782160bf43e993
Den <dimas.dermawan@ui.ac.id>
2023-05-08 23:04:49 +0700

    Adding, organizing, and renaming assets

---
+++ src/assets/footer/social-media/index.js
@@ -0,0 +1,28 @@
+import ig from "./ig.svg"
+import lin from "./lin.svg"
+import line from "./line.svg"
+import mail from "./mail.svg"
+import twt from "./twt.svg"
+
+export default [
+    {
+      link: "https://www.instagram.com",
+      img: ig,
+    },
+    {
+      link: "https://www.linkedin.com",
+      img: lin,
+    },
+    {
+      link: "https://www.line.me",
+      img: line,
+    },
+    {
+      link: "https://www.twitter.com",
+      img: twt,
+    },
+    {
+      link: "https://www.gmail.com",
+      img: mail,
+    },
+  ]
