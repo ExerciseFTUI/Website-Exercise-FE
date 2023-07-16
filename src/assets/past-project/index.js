40b1d4a3c0fa9051cb078a31e6fcfa4a515243f7
Eriqo90AW <eriqo.arief@ui.ac.id>
2023-07-16 20:17:47 +0700

    merge newest past projects from harris/get-to-know

---
+++ src/assets/past-project/index.js
@@ -0,0 +1,50 @@
+import tokopedia from './tokopedia.png'
+import flip from './flip.png'
+
+export default [
+    {
+        logo: tokopedia,
+        name: "E-Commerce Product Web Scraping",
+        x:"x",
+        comp:"Tokopedia",
+        desc:"Build a desktop GUI Application with Python programming language to create a web scraping system based on Selenium. The goal of this project is to gather data for Data Analytics purposes to predict future behavior.",
+        developer:"Mbak Ryan",
+        devdesc:"Desainer Handal"
+    },
+    {
+        logo: flip,
+        name: "Fintech Automation System",
+        x:"x",
+        comp:"Flip",
+        desc:"Build a desktop GUI Application with Python programming language to create a web scraping system based on Selenium. The goal of this project is to gather data for Data Analytics purposes to predict future behavior.",
+        developer:"Mbak Ryan",
+        devdesc:"Desainer Handal"
+    },
+    {
+        logo: null,
+        name: "Temperature Detection with facial detection and IoT",
+        x:"",
+        comp:"",
+        desc:"Developed an IoT projects to help the Electrical Department on creating a device to measure each person temperature when the device detected a valid face from the camera. The data will also be sent into an endpoint to be collected and analyzed.",
+        developer:"Mbak Ryan",
+        devdesc:"Desainer Handal"
+    },
+    {
+        logo: null,
+        name: "Document Conversion and P12 Signing",
+        x:"",
+        comp:"",
+        desc:"Develop a document conversion program to convert docx file format to pdf. The P12 signing will make a timestamp as a mark on the pdf signing. This program created with Python, Java, Django, and Spring REST API methods.",
+        developer:"Mbak Ryan",
+        devdesc:"Desainer Handal"
+    },
+    {
+        logo: null,
+        name: "Pothole Detector",
+        x:"",
+        comp:"",
+        desc:"Develop an Embedded System developed to detect several potholes in a certain area by integrating it into an autonomous vehicle.",
+        developer:"Mbak Ryan",
+        devdesc:"Desainer Handal"
+    }
+]
