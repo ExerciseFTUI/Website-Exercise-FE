cea73eed74cc7e68e5d1a167080961e5c38f94e7
Eriqo90AW <eriqo.arief@ui.ac.id>
2023-07-30 17:52:28 +0700

    still not working

---
+++ src/assets/event-details/details/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const digSilent = {
+    title: "Power Factory DigSILENT Training",
+    price: {
+        indv:100,
+        team:450
+    },
+    date: "29 April 2023",
+    desc: "Digsilent Training is a training with the DigSilent application specifically for students majoring in Electrical Engineering who want to take Electric Power specialization. Digsilent Training collaborates with STL Laboratory FTUI in March 2023.",
+    benefits: "Kompetensi Dasar STL dan DigSILENT",
+    
+}
+
+export default index
